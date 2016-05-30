74e305e5446ff9d4f35ef51966e9721ca6d583e8
chenyuchao <yuchao.chen@hand-china.com>
2016-05-30 09:22:50 +0800

    陈宇超5月30日提交

---
+++ app/pages/application/dorm-apply/dorm-apply.js
@@ -0,0 +1,33 @@
+/**
+ * Created by LeonChan on 2016/5/27.
+ */
+angular.module('myApp')
+  .config(['$stateProvider',
+    function ($stateProvider) {
+      $stateProvider
+        .state('tab.dorm-apply', {
+          url: '/dorm-apply',
+          views: {
+            'tab-application': {
+              templateUrl: 'build/pages/application/dorm-apply/dorm-apply.html',
+              controller: 'DormApplyCtrl'
+            }
+          }
+        })
+    }]);
+angular.module('applicationModule')
+  .controller('DormApplyCtrl', [
+    '$scope',
+    '$state',
+    'baseConfig',
+    '$ionicHistory',
+    function ($scope,
+              $state,
+              baseConfig,
+              $ionicHistory) {
+
+     $scope.goBack=function(){//返回按钮
+       $ionicHistory.goBack();
+     }
+
+    }]);
