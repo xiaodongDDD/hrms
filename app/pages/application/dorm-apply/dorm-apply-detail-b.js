4d0047fdf7e34f3e93623b37207321f0d03687f6
chenyuchao <yuchao.chen@hand-china.com>
2016-06-13 10:56:00 +0800

    住宿申请部分提交

--- app/pages/application/dorm-apply/dorm-apply-detail-b.js
+++ app/pages/application/dorm-apply/dorm-apply-detail-b.js
@@ -22,26 +22,67 @@ angular.module('applicationModule')
     'baseConfig',
     '$ionicHistory',
     'dormApplyTypeService',
+    'hmsHttp',
+    'hmsPopup',
+    '$rootScope',
+    '$timeout',
     function ($scope,
               $state,
               baseConfig,
               $ionicHistory,
-              dormApplyTypeService) {
-      var applyType = dormApplyTypeService.getInfo();
+              dormApplyTypeService,
+              hmsHttp,
+              hmsPopup,
+              $rootScope,
+              $timeout) {
+      $scope.applyInfo = dormApplyTypeService.getInfo();
       $scope.checkIn = false;//审批中状态标志位
       $scope.checkOut = false;//已拒绝状态标识位
       $scope.buttonText = ""//按钮上显示的文字
-      if (applyType == "checkIn") {//已入住
+      if ($scope.applyInfo.status == "已入住") {//已入住
         $scope.checkIn = true;
         $scope.checkOut = false;
         $scope.buttonText = "续住";
-      } else if (applyType == "checkOut") {//已退房
+      } else if ($scope.applyInfo.status == "已退房") {//已退房
         $scope.checkIn = false;
         $scope.checkOut = true;
         $scope.buttonText = "再次预定";
       }
-      $scope.goBack = function () {
+      $scope.goBack = function () {//返回上一界面
         $ionicHistory.goBack();
+      };
+
+      $scope.renewContract=function(){//续住
+        var url=baseConfig.businessPath+"/wfl_apply_room/overstay_apply_room";
+        var param={
+          "params": {
+            p_employee_number:window.localStorage.empno,
+            p_checkin_date:"20160815",
+            p_checkout_date:"20160920",
+            p_room_number:$scope.applyInfo.roomNumber,
+            p_bed_number:$scope.applyInfo.bedNumber,
+            p_apply_type:$scope.applyInfo.applyType,
+            p_reason:""
+          }
+        };
+        hmsPopup.showLoading('请稍候');
+        hmsHttp.post(url,param).success(function(result){
+          hmsPopup.hideLoading();
+          var message=result.message;
+          hmsPopup.showShortCenterToast(message);
+          if(result.status=="S"){
+              $rootScope.$broadcast("APPLY_SUCCESS");//触发上一界面重新刷新数据
+              $ionicHistory.goBack();//删除申请成功后返回上一界面
+          }
+          if (baseConfig.debug) {
+            console.log("result success " + angular.toJson(result));
+          }
+        }).error(function(error,status){
+          hmsPopup.hideLoading();
+          if (baseConfig.debug) {
+            console.log("response error " + angular.toJson(error));
+          }
+        });
       };
 
     }]);
--- app/pages/application/dorm-apply/dorm-apply-vacant-room.js
+++ app/pages/application/dorm-apply/dorm-apply-vacant-room.js
@@ -18,49 +18,59 @@ angular.module('myApp')
 angular.module('applicationModule')
   .controller('DormApplyVacantRoomCtrl', [
     '$scope',
+    '$rootScope',
     '$state',
     'baseConfig',
     '$ionicHistory',
-    'dormApplyTypeService',
+    'dormApplySearchResultService',
+    'hmsHttp',
+    'hmsPopup',
+    '$timeout',
     function ($scope,
+              $rootScope,
               $state,
               baseConfig,
               $ionicHistory,
-              dormApplyTypeService) {
-     $scope.roomlist=[
-       {
-        room_number:"116",
-        floor_number:"1",
-        bed_number:"2",
-        dorm_type:"男"
-       },
-       {
-         room_number:"226",
-         floor_number:"2",
-         bed_number:"1",
-         dorm_type:"男"
-       },
-       {
-         room_number:"326",
-         floor_number:"3",
-         bed_number:"1",
-         dorm_type:"男"
-       },
-       {
-         room_number:"338",
-         floor_number:"3",
-         bed_number:"2",
-         dorm_type:"男"
-       },
-       {
-         room_number:"515",
-         floor_number:"5",
-         bed_number:"1",
-         dorm_type:"男"
-       }
-     ];
+              dormApplySearchResultService,
+              hmsHttp,
+              hmsPopup,
+              $timeout) {
+
+     var resultInfo=dormApplySearchResultService.getInfo();//从service中拿到查询界面得到的查询结果
+     $scope.roomlist=resultInfo.result;
      $scope.goBack=function(){//返回上一界面
        $ionicHistory.goBack();
      };
-
+     $scope.applyRoom=function(num){
+       var url=baseConfig.businessPath+"/wfl_apply_room/apply_room";
+       var param={
+         "params": {
+           p_employee_number:window.localStorage.empno,
+           p_checkin_date:resultInfo.checkinDate,
+           p_checkout_date:resultInfo.checkoutDate,
+           p_room_number:$scope.roomlist[num].room_number,
+           p_bed_number:$scope.roomlist[num].bed_number,
+           p_apply_type:resultInfo.applyType,
+           p_reason:""
+         }
+       };
+       hmsPopup.showLoading('请稍候');
+       hmsHttp.post(url,param).success(function(result){
+         hmsPopup.hideLoading();
+         var message=result.message;
+         hmsPopup.showShortCenterToast(message);
+         if(result.status=="S"){//如果申请成功的话
+             $ionicHistory.goBack(-2);//返回住宿申请界面
+             $rootScope.$broadcast("APPLY_SUCCESS");//自动触发刷新历史申请数据
+         }
+         if (baseConfig.debug) {
+           console.log("result success " + angular.toJson(result));
+         }
+       }).error(function(error,status){
+         hmsPopup.hideLoading();
+         if (baseConfig.debug) {
+           console.log("response error " + angular.toJson(error));
+         }
+       });
+     };
     }])
