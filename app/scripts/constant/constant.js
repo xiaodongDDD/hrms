bda0437fd1fa7e907b3474f261284b61c6186253
daiwen <daiwen@example.com>
2016-07-01 10:56:04 +0800

    update

---
+++ app/scripts/constant/constant.js
@@ -0,0 +1,10 @@
+/**
+ * Created by wolf on 2016/7/1.
+ * @description: add common alert for every module--
+ * @attention:  nomenclature and block
+ */
+'use strict';
+angular.module("HmsModule", [])
+  .constant('hmsConstant',[function () {
+
+  }]);
