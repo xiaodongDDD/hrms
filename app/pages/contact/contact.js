5da2d19dbc8c6f97ea19f0625fd9f4aa55278bd4
daiwen <daiwen@example.com>
2016-07-06 21:26:13 +0800

    update /7/6

--- app/pages/contact/contact.js
+++ app/pages/contact/contact.js
@@ -27,12 +27,14 @@ angular.module('contactModule')
         $scope.showInfinite = false; //默认隐藏无限滚动的标签
         $scope.contactLoading = false; //默认不显示loading加载
         $scope.showHistory = true; //默认显示搜索历史
+        $scope.showClear = false; //默认隐藏搜索框的清楚按钮
         $scope.resultList = []; //存储搜索结果
         $scope.contactKey = {getValue: ''}; //绑定输入的关键字
         $scope.historys = []; //存储搜索历史的关键字
         $scope.newPage = 0;
         var CONTACT_TAG = 'contact:\n';
         var DB_NAME = 'key_history';
+        var LINK_MAN = 'common_linkman';
         var position = ''; //记录滚动条的位置--
         var getEmployeeUrl = baseConfig.businessPath + '/get_empinfo/get_employees';
         var employeeParams = {params: {p_token: '', p_page: ''}};
@@ -42,7 +44,19 @@ angular.module('contactModule')
         }
       }
 
+      function getCommonLinkMan() { //获取常用联系人
+        $scope.customContactsInfo = (storedb(LINK_MAN).find()).arrUniq();
+        if ($scope.customContactsInfo.length > 15) {
+          $scope.customContactsInfo = $scope.customContactsInfo.slice(0, 15);
+        }
+      };
+
       $scope.$on('$ionicView.enter', function (e) {
+        getCommonLinkMan();
+      });
+
+      $scope.$on('REFRESH_LINKMAN', function (e) {
+        getCommonLinkMan();
       });
 
       $scope.$on('$destroy', function (e) {
@@ -78,6 +92,19 @@ angular.module('contactModule')
         }
       };
 
+      function dealCommonLinkMan(newObject) { //常用联系人最多15个
+        storedb(LINK_MAN).insert(newObject, function (err) {
+          if (!err) {
+            $scope.customContactsInfo = (storedb(LINK_MAN).find()).arrUniq();
+          } else {
+            hmsPopup.showShortCenterToast(err);
+          }
+        });
+        if ($scope.customContactsInfo.length > 15) {
+          $scope.customContactsInfo = $scope.customContactsInfo.slice(0, 15);
+        }
+      };
+
       /**
        * modal input 方法区
        */
@@ -94,10 +121,12 @@ angular.module('contactModule')
       };
 
       $scope.hideContactSearch = function () {
-        $scope.contactInputModal.hide();
         $scope.showHistory = true;
+        $scope.showClear = false;
         $scope.resultList = [];
         $scope.contactKey.getValue = '';
+        $scope.$emit('REFRESH_LINKMAN');
+        $scope.contactInputModal.hide();
       };
 
       $scope.getEmployeeData = function (moreFlag) { //获取搜索关键字的数据
@@ -141,12 +170,19 @@ angular.module('contactModule')
         $scope.getEmployeeData('loadMore');
       };
 
+      $scope.clearInputContent = function () {
+        $scope.contactKey.getValue = '';
+        $scope.searchContacts();
+      };
+
       $scope.searchContacts = function () { //响应搜索输入框的方法
         $scope.showHistory = false;
         if ($scope.contactKey.getValue === '') {
           $scope.showHistory = true;
           $scope.resultList = [];
+          $scope.showClear = false;
         } else {
+          $scope.showClear = true;
         }
         $scope.newPage = 1;
         employeeParams.params.p_token = $scope.contactKey.getValue;
@@ -162,6 +198,7 @@ angular.module('contactModule')
         employeeParams.params.p_page = 1;
         $scope.contactLoading = true;
         $scope.showHistory = false;
+        $scope.showClear = true;
         $scope.resultList = [];
         dealHistory(values.historyItem);
         $scope.getEmployeeData('init');
@@ -181,14 +218,22 @@ angular.module('contactModule')
       $scope.goStructure = function () {
       };
 
-      $scope.goDetailInfo = function () {
+      $scope.goDetailInfo = function (newEmployeeNumber) {
         $scope.contactInputModal.hide();
-        $state.go('tab.employeeDetail', {employeeNumber: '7963'});
+        $state.go('tab.employeeDetail', {employeeNumber: newEmployeeNumber});
       };
 
-      $scope.telNumber = function (event, newNumber) {
+      $scope.telNumber = function (event, newNumber, newName, newEmpNumber, flag) {
         event.stopPropagation(); //阻止事件冒泡
         window.location.href = "tel:" + newNumber;
+        if (flag === 'search') {
+          var employeeBaseInfo = {
+            tel: newNumber,
+            name: newName,
+            employeeNumber: newEmpNumber
+          };
+          dealCommonLinkMan(employeeBaseInfo);
+        }
       };
 
     }]);
