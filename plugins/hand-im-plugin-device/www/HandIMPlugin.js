28a27214444643fe8589e94d39acc849061527ea
daiwen <daiwen@example.com>
2016-07-09 19:02:00 +0800

    add plugin

---
+++ plugins/hand-im-plugin-device/www/HandIMPlugin.js
@@ -0,0 +1,73 @@
+﻿var exec = require('cordova/exec');
+
+var HandIMPlugin = {
+//跳转到会话列表
+getChatList : function(successCallback, errorCallback, options) {
+	if (errorCallback == null) { errorCallback = function() {}}
+    if (typeof errorCallback != "function")  {
+
+        return
+    }
+    if (typeof successCallback != "function") {
+
+        return
+    }
+    exec(successCallback, errorCallback, "HandIMPlugin", "getChatList", [options]);
+
+},
+//跳转到会话界面
+toChatAct : function(successCallback, errorCallback, options) {
+	if (errorCallback == null) { errorCallback = function() {}}
+    if (typeof errorCallback != "function")  {
+
+        return
+    }
+    if (typeof successCallback != "function") {
+
+        return
+    }
+    exec(successCallback, errorCallback, "HandIMPlugin", "toChatAct", [options]);
+}
+};
+
+//跳转到会话列表
+/*dojs.prototype.getChatList = function(successCallback, errorCallback, options) {
+	if (errorCallback == null) { errorCallback = function() {}}
+    if (typeof errorCallback != "function")  {
+
+        return
+    }
+    if (typeof successCallback != "function") {
+
+        return
+    }
+    cordova.exec(successCallback, errorCallback, "HandIMPlugin", "getChatList", options);
+
+};
+//跳转到会话界面
+dojs.prototype.toChatAct = function(successCallback, errorCallback, options) {
+	if (errorCallback == null) { errorCallback = function() {}}
+    if (typeof errorCallback != "function")  {
+
+        return
+    }
+    if (typeof successCallback != "function") {
+
+        return
+    }
+    cordova.exec(successCallback, errorCallback, "HandIMPlugin", "toChatAct", options);
+};
+
+
+
+//-------------------------------------------------------------------
+if(!window.plugins) {
+    window.plugins = {};
+}
+if (!window.plugins.do) {
+    window.plugins.dojs = new dojs();
+}*/
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = HandIMPlugin;
+}
