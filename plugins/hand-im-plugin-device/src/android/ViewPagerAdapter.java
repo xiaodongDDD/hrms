778c6c198917816c128b0b86e14a22091767efe4
gusenlin <gusenlin@gusenlindeMacBook-Pro.local>
2016-07-11 00:02:43 +0800

    senlin

--- plugins/hand-im-plugin-device/src/android/ViewPagerAdapter.java
+++ plugins/hand-im-plugin-device/src/android/ViewPagerAdapter.java
@@ -5,7 +5,7 @@ import java.util.List;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
-import com.hand.china_hrms.R;
+import com.hand_china.hrms.R;
 
 public class ViewPagerAdapter extends PagerAdapter {
 
