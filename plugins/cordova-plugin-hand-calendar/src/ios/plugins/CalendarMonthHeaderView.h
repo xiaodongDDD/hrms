5941a2d414ece204f3d014651ca70629c881136d
daiwen <daiwen@example.com>
2016-07-12 21:03:54 +0800

    update

---
+++ plugins/cordova-plugin-hand-calendar/src/ios/plugins/CalendarMonthHeaderView.h
@@ -0,0 +1,16 @@
+//
+//  ETIMonthHeaderView.h
+//  LYJCanlender
+//
+//
+//  Created by Liyanjun on 16/6/7.
+//  Copyright © 2016年 liyanjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarMonthHeaderView : UICollectionReusableView
+
+@property (weak, nonatomic) UILabel *masterLabel;
+
+@end
---
+++ plugins/cordova-plugin-hand-calendar/src/ios/plugins/Color.h
@@ -0,0 +1,35 @@
+//
+//  Color.h
+//
+//
+//  Created by Liyanjun on 16/6/7.
+//  Copyright © 2016年 liyanjun. All rights reserved.
+//
+
+
+/**
+ 
+ 所有自定义的颜色请写在 MyRes  》 Color 中并注释上颜色名称
+ 
+ */
+#import "UtilsMacro.h"
+
+#ifndef VIPTravel_Color_h
+
+#define COLOR_THEME1 ([UIColor blackColor])//工作日颜色
+#define COLOR_THEME ([UIColor blackColor])//周末颜色
+
+#define COLOR_HOLODAT  (RGB(115,187,162))//节假日颜色
+#define COLOR_TITLE  (RGB(35,83,154))//年月颜色
+#define COLOR_WORK  (RGB(185,87,86))//上班颜色
+//#define COLOR_THEME ([UIColor blackColor])//自定义颜色
+
+#define VIPTravel_Color_h
+
+#define totalDays 365*4 //一年的天数
+
+#endif
+
+
+
+
