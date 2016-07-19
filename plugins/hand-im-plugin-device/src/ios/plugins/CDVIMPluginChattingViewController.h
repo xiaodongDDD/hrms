b6cbd04ac52e85f2f8e154385643dca6c47a20c1
gusenlin <senlin.gu@hand-china.com>
2016-07-19 22:53:43 +0800

    senlin

---
+++ plugins/hand-im-plugin-device/src/ios/plugins/CDVIMPluginChattingViewController.h
@@ -0,0 +1,14 @@
+//
+//  CDVIMPluginChattingViewController.h
+//  HelloCordova
+//
+//  Created by wangsheng on 16/7/11.
+//
+//
+
+#import <RongIMKit/RongIMKit.h>
+
+@interface CDVIMPluginChattingViewController : RCConversationViewController
+@property (nonatomic,copy)NSString *navTitle;
+@property (nonatomic,copy)NSString *target_id;
+@end
