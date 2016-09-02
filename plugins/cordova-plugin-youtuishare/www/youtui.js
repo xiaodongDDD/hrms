da589187fd8e345a0c072705b5074dfa36613799
luyufei <yufei.lu@hand-china.com>
2016-09-02 19:27:36 +0800

    youtui share

---
+++ plugins/cordova-plugin-youtuishare/www/youtui.js
@@ -0,0 +1,37 @@
+var exec = require('cordova/exec');
+var youtuiShare = function() {
+};
+
+
+youtuiShare.prototype.share = function(successCallback, errorCallback, options) {
+    console.log("youtuiShare");
+    if (errorCallback == null) { errorCallback = function() {}}
+    if (typeof errorCallback != "function")  {
+
+        return
+    }
+    if (typeof successCallback != "function") {
+        
+        return
+    }
+    exec(successCallback, errorCallback, "YouTui", "share", options);
+};
+
+if(!window.plugins) {
+    window.plugins = {};
+}
+
+if (!window.plugins.youtuiShare) {
+    alert("No window.plugins.youtuiShare.");
+    window.plugins.youtuiShare = new youtuiShare();
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = youtuiShare;
+}
+
+//module.exports = {
+//    share: function (successCallback, errorCallback, options) {
+//        exec(successCallback, errorCallback, 'YouTui', 'share', options);
+//    }
+//};
