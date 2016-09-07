083c29b959c9d3dacdbe53aa5b69d24b56b1aacd
chenyuchao <yuchao.chen@hand-china.com>
2016-09-07 21:33:17 +0800

    机票申请-乘机受益人部分修改了逻辑

--- app/pages/application/flight-ticket/flight-passenger-list.js
+++ app/pages/application/flight-ticket/flight-passenger-list.js
@@ -27,6 +27,7 @@ angular.module('applicationModule')
     'hmsHttp',
     'baseConfig',
     'passengerService',
+    '$ionicModal',
     function($scope,
              $ionicHistory,
              $http,
@@ -35,33 +36,21 @@ angular.module('applicationModule')
              $timeout,
              hmsHttp,
              baseConfig,
-             passengerService){
+             passengerService,
+             $ionicModal){
+      $ionicModal.fromTemplateUrl('build/pages/application/flight-ticket/modal/passenger-relationship-select-modal.html', {//定义关系选择modal
+        scope: $scope
+      }).then(function (modal1) {
+        $scope.chooseRelationshipModal = modal1;
+      });
       $scope.editMode = false;//编辑模式
       $scope.createMode = false;//创建模式
       $scope.showData = false;//默认有数据
       $scope.showLoading=false;//默认加载数据
       $scope.editItem=[];//编辑选项
       $scope.createItem=[];//创建选项
-      //$scope.passengerList=[//乘客列表
-      //  {
-      //    passenger_name:"刘亦菲",
-      //    passenger_gender:"female",
-      //    passenger_id:"898876780129092109",
-      //    passenger_passport:"CU7897"
-      //  },
-      //  {
-      //    passenger_name:"朱丽叶",
-      //    passenger_gender:"female",
-      //    passenger_id:"7816289009812892",
-      //    passenger_passport:"NH8719"
-      //  },
-      //  {
-      //    passenger_name:"尚文",
-      //    passenger_gender:"male",
-      //    passenger_id:"890129309182987654",
-      //    passenger_passport:"LK9012"
-      //  }
-      //];
+      $scope.relationshipList=["父亲","母亲","哥哥","弟弟","姐姐","妹妹","女儿","儿子","妻子","丈夫","其他"];//乘机人关系列表
+      var chooseRelationMode="";//选择乘机人关系的模式，编辑还是新建
       $scope.passengerList=[];
       $scope.passengerList=passengerService.getPassengerList().concat();//拿到受益人列表
       $scope.passengerList.splice(0,1);
@@ -120,7 +109,34 @@ angular.module('applicationModule')
         }
         return result;
       };
-      $scope.judgePassengerGender=function(num){
+      $scope.judgeRelationship=function(param){//编辑模式下判断乘机人关系
+        var result="";
+        if(param == 1){
+          result="父亲";
+        }else if(param == 2){
+          result="母亲";
+        }else if(param == 3){
+          result="哥哥";
+        }else if(param == 4){
+          result="姐姐";
+        }else if(param == 5){
+          result="弟弟";
+        }else if(param == 6){
+          result="妹妹";
+        }else if(param == 15){
+          result="女儿";
+        }else if(param == 16){
+          result="儿子";
+        }else if(param == 17){
+          result="妻子";
+        }else if(param == 18){
+          result="丈夫";
+        }else if(param == 99){
+          result="其他";
+        }
+        return result;
+      };
+      $scope.judgePassengerGender=function(num){//判断乘机人性别
         var param=$scope.passengerList[num].passenger_gender;
         var result="";
         if(param == "male"){
@@ -129,7 +145,35 @@ angular.module('applicationModule')
           result="女";
         }
         return result;
-      }
+      };
+      $scope.judgePassengerRelationship=function(num){//判断乘机人关系
+        var param=$scope.passengerList[num].relation_code;
+        var result="";
+        if(param == 1){
+          result="父亲";
+        }else if(param == 2){
+          result="母亲";
+        }else if(param == 3){
+          result="哥哥";
+        }else if(param == 4){
+          result="姐姐";
+        }else if(param == 5){
+          result="弟弟";
+        }else if(param == 6){
+          result="妹妹";
+        }else if(param == 15){
+          result="女儿";
+        }else if(param == 16){
+          result="儿子";
+        }else if(param == 17){
+          result="妻子";
+        }else if(param == 18){
+          result="丈夫";
+        }else if(param == 99){
+          result="其他";
+        }
+        return result;
+      };
       $scope.switchEditGender=function(param){//编辑模式下切换性别
         if(param == "female"){
           $scope.editItem[0].passenger_gender = "female";
@@ -150,6 +194,7 @@ angular.module('applicationModule')
         var param={
           passenger_name:chosenItem.passenger_name,
           passenger_gender:chosenItem.passenger_gender,
+          relation_code:chosenItem.relation_code,
           passenger_id:chosenItem.passenger_id,
           passenger_passport:chosenItem.passenger_passport
         };
@@ -157,6 +202,45 @@ angular.module('applicationModule')
       };
       $scope.cannotEditId=function(){//提示乘机人身份证号不可以编辑
        hmsPopup.showVeryShortCenterToast("乘机人身份证号是不可编辑字段");
+      };
+      $scope.editRelationship=function(param){//编辑乘机人关系
+        chooseRelationMode=param;
+        $scope.chooseRelationshipModal.show();
+      };
+      function relationTextToCode(param){
+        var result="";
+        if(param=="父亲"){
+          result=1;
+        }else if(param=="母亲"){
+          result=2;
+        }else if(param=="哥哥"){
+          result=3;
+        }else if(param=="姐姐"){
+          result=4;
+        }else if(param=="弟弟"){
+          result=5;
+        }else if(param=="妹妹"){
+          result=6;
+        }else if(param=="女儿"){
+          result=15;
+        }else if(param=="儿子"){
+          result=16;
+        }else if(param=="妻子"){
+          result=17;
+        }else if(param=="丈夫"){
+          result=18;
+        }else if(param=="其他"){
+          result=99;
+        }
+        return result;
+      }
+      $scope.finishChoosingRelationship=function(param){//乘机人关系编辑完毕
+        if(chooseRelationMode=="create"){//新建模式
+         $scope.createItem[0].relation_text=param;
+        }else if(chooseRelationMode=="edit"){//编辑模式
+         $scope.editItem[0].relation_code = relationTextToCode(param);
+        }
+        $scope.chooseRelationshipModal.hide();
       };
       $scope.deletePassenger=function(num){//删除乘机受益人或取消编辑模式
           hmsPopup.confirm("确认删除本乘机受益人？","提示",function(){
@@ -205,7 +289,8 @@ angular.module('applicationModule')
           passenger_name:"",
           passenger_gender:"male",//默认男
           passenger_id:"",
-          passenger_passport:""
+          passenger_passport:"",
+          relation_text:"请选择乘机人关系"
         };
         $scope.createItem.push(param);
       };
@@ -231,6 +316,7 @@ angular.module('applicationModule')
                p_employee_number:window.localStorage.empno,
                p_passenger_name:$scope.editItem[0].passenger_name,
                p_passenger_id:$scope.editItem[0].passenger_id,
+               p_relation_code:$scope.editItem[0].relation_code,
                p_passenger_passport:$scope.editItem[0].passenger_passport,
                p_passenger_gender:"",
                p_cancel_code:2
@@ -247,6 +333,7 @@ angular.module('applicationModule')
              hmsPopup.showLongCenterToast(result.returnMsg);
              if(result.status == "S"){
                $scope.editMode=false;
+               $scope.editItem=[];
                initPassengerList();
              }else if(result.status == "E"){
 
@@ -263,7 +350,9 @@ angular.module('applicationModule')
            hmsPopup.showVeryShortCenterToast("乘机人姓名不能为空");
          }else if(createValue.passenger_id == ""){
            hmsPopup.showVeryShortCenterToast("乘机人身份证号不能为空");
-         }else if(createValue.passenger_name!="" && createValue.passenger_id!=""){//创建完成
+         }else if(createValue.relation_text == "请选择乘机人关系"){
+           hmsPopup.showVeryShortCenterToast("请选择乘机人关系");
+         }else if(createValue.passenger_name!="" && createValue.passenger_id!="" && createValue.relation_text!="请选择乘机人关系"){//创建完成
            $scope.showLoading=true;
            var url=baseConfig.businessPath+"/ticket_apply_info/create_beneficiaris";
            var param={
@@ -271,6 +360,7 @@ angular.module('applicationModule')
                p_employee_number:window.localStorage.empno,
                p_passenger_name:$scope.createItem[0].passenger_name,
                p_passenger_id:$scope.createItem[0].passenger_id,
+               p_relation_code:relationTextToCode($scope.createItem[0].relation_text),
                p_passenger_passport:$scope.createItem[0].passenger_passport,
                p_passenger_gender:"",
                p_cancel_code:1
@@ -287,6 +377,7 @@ angular.module('applicationModule')
              hmsPopup.showLongCenterToast(result.returnMsg);
              if(result.status == "S"){
                $scope.createMode=false;
+               $scope.createItem=[];
                initPassengerList();
              }else if(result.status == "E"){
 
