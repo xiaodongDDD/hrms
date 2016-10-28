40d34c3ece6f6b9a502ed5ed2280b81ac52c94c4
[sunbohao] <[bohao.sun@hand-china.com]>
2016-10-28 16:04:30 +0800

    修改ios样式bug

--- plugins/hand-im-plugin-device/src/android/contact/DataCheckCallBack.java
+++
@@ -1,8 +0,0 @@
-package com.hand.im.contact;
-
-/**
- * Created by panx on 2016/9/24.
- */
-public interface DataCheckCallBack {
-    public void setCheckInfo();
-}
--- plugins/hand-im-plugin-device/src/ios/plugins/DataModels.h
+++
@@ -1,8 +0,0 @@
-//
-//  DataModels.h
-//
-//  Created by wintel sui on 16/8/18
-//  Copyright (c) 2016 Chinasofti. All rights reserved.
-//
-
-#import "CallDirectoryPhoneModel.h"
