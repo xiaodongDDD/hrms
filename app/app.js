9638b2e203b9118db8e7e470c57fafd019ea587c
handdev <senlin.gu@hand-china.com>
2016-11-21 20:49:15 +0800

    senlin

--- app/app.js
+++ app/app.js
@@ -58,6 +58,8 @@ angular.module('myApp')
     ) {
 
       $ionicPlatform.ready(function () {
+
+        splashscreen.hide();
         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
         // for form inputs)
         if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
