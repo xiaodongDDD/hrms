dcc6eef930161142146dd17b27be66541890e78a
wangkaihua <kaihua.wang@hand-china.com>
2017-01-01 18:14:36 +0800

    'wkh'

--- app/pages/application/customers/customer-detail/customer-detail-clue/customer-detail-clue.js
+++ app/pages/application/customers/customer-detail/customer-detail-clue/customer-detail-clue.js
@@ -9,12 +9,14 @@ angular.module('customerModule')
     '$timeout',
     'customerOpportunityService',
     'customerDetailService',
+    '$ionicHistory',
     function ($scope,
               $state,
               hmsPopup,
               $timeout,
               customerOpportunityService,
-              customerDetailService) {
+              customerDetailService,
+              $ionicHistory) {
 
       $scope.page = 1;
       $scope.pageSize = 10;
@@ -40,7 +42,12 @@ angular.module('customerModule')
       };
 
       $scope.goDetail=function(para) {
-        $state.go("tab.clue-detail",{data:para});
+       /* $state.go("tab.clue-detail",{data:para});*/
+        if($ionicHistory.viewHistory().backView.stateName=="tab.contactDetail"){
+          $state.go('tab.clue-detail2', {data: para});
+        }else{
+          $state.go('tab.clue-detail', {data: para});
+        }
       };
 
       customerOpportunityService.getOpportunities(initClueSuccess,{page:$scope.page,pageSize:$scope.pageSize,customerId: $scope.customerId,opportunityType:"HCRM_CLUE"});
