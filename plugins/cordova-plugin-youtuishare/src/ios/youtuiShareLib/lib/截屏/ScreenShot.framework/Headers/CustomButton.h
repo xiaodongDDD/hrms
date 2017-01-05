6d1081607e35a3becd18a888105030351175e0b8
handdev <senlin.gu@hand-china.com>
2017-01-05 22:09:09 +0800

    senlin

---
+++ plugins/cordova-plugin-youtuishare/src/ios/youtuiShareLib/lib/截屏/ScreenShot.framework/Headers/CustomButton.h
@@ -0,0 +1,13 @@
+//
+//  CustomButton.h
+//  DrawerTest
+//
+//  Created by mac on 14-11-19.
+//  Copyright (c) 2014年 youtui.mobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomButton : UIButton
+
+@end
